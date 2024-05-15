3207d4deb4065b5d3451002fc8d633f918cbfe78
Laiba Qazi <164245574+qazilaiba08@users.noreply.github.com>
2024-05-15 16:00:10 +0500

    javscript

---
+++ script.js
@@ -0,0 +1,168 @@
+
+
+// a++ ;//post increment
+// ++a ; //pre increment
+
+// let a = 10;
+// // console.log(++a);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+
+// let number1 = +prompt('Enter Your First Number',20);
+// let number2 = +prompt('Enter Your Second Number');
+// console.log(number1 + number2);
+
+//opreters +=;
+//a +=2;
+// a = a + 2;
+// a = a * 2;
+// a = a % 2 ;
+// a = a / 2;
+// 2 ** 2;
+
+//comparsion opreteros
+
+// let f = 18;
+// let z = 18;
+// console.log(f!==z);
+
+// let mode = "yellow";
+// let color = "black";
+// if(mode == "dark"){
+//     console.log(color); 
+// }
+//  else{
+//     console.log("white");
+//  }
+// let hello ;
+// console.log(hello);
+
+// let age = +prompt("Enter Your Age");
+// if(age >= 18){
+//     console.log('You are eligable for voting');
+// }
+// else{
+//     console.log("You are not eligable for voting");
+// }
+
+// let number = +prompt('Enter Your Number');
+// if (number % 2 == 0) {
+//     console.log( number,"is an even number");
+// }
+// else{
+//     console.log(number, "is an odd number");
+// }
+
+// let age = +prompt('Enter Your Age');
+// if(age < 18){
+//     console.log("Junior");
+// }
+// else if(age > 60){
+//     console.log("Senior");
+// }
+// else{
+//     console.log("Middle");
+// }
+// let num = +prompt("Enter Your Number");
+// if(num % 5 == 0){
+//     console.log(num,"is a multiple of 5");
+// }
+// else{
+//     console.log(num,"is not multiple of 5");
+// }
+
+//class Activaty
+
+//Q 1
+
+// let age = +prompt("Enter Your Age");
+// if(age > 18 ){
+//     console.log('You are eligable for voting');
+// }
+// else{
+//     console.log("You are not eligable for voting");
+// }
+
+// // //Q 2
+
+// let tempreture = +prompt("INPUT the tempreture");
+// if(temp > 25){
+//     console.log("Its a hot day");
+// }
+// else{
+//     console.log("Its a normal day");
+// }
+
+// //Q 3
+// let student_grade=+prompt("Enter your score")
+// if(score >=60){
+//     console.log("You passed the test");
+// }
+// else{
+//     console.log("you failed the test");
+// }
+
+// // //Q 4
+
+
+// let raining = true;
+
+// if(raining == false){
+//     console.log("remember to bring an umberalla"); 
+// }
+//  else{
+//     console.log("Enjoy the sunny weather");
+//  }
+
+// //Q5
+
+// let age = +prompt("Enter your age");
+// if(age >= 18){
+//     console.log("You are an adult");
+// }
+// else{
+//     console.log("you are a minor");
+// }
+// //Q 6
+
+// let number = +prompt("Enter the number");
+// if(number > 0){
+//     console.log("The number is poistive");
+// }
+// else if (number < 0) {
+//     console.log("The number is negative");
+// }
+// else if(number == 0){
+//     console.log("The number is zero");
+// }
+// else{
+//     console.log("The number is inetger");
+// }
+
+// logical opreter
+// let score = +prompt("Enter your score");
+// if (score >= 80 && score >= 60) {
+//     console.log("You are passed");
+// }
+// else{
+//     console.log("you are failed");
+// }
+
+//logical OR 
+// let score = +prompt("Enter your score");
+// if (score >= 80  || score <= 60) {
+//     console.log("You are passed");
+// }
+// else{
+//     console.log("you are failed");
+// }
+//logical not!
+let score = +prompt("Enter your score");
+if (score >= 80 || score <= 60) {
+    console.log("You are passed");
+}
+else{
+    console.log("you are failed");
+}
